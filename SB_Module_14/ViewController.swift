2f5ce84502ebe7aa4172a6e84ecacf4ce3b73a1a
Mogew <alimovren@gmail.com>
2023-05-07 11:48:17 +0600

    Initial Commit

---
+++ SB_Module_14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB_Module_14
+//
+//  Created by Renat on 07.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
